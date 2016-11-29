116aca6e952fa9184ade97c6f9fe436adcf84d7a
Zoe Stuart <zoestuart13@gmail.com>
2016-11-29 15:47:08 -0500

    added stages - start, playing, and end screens

--- maze.py
+++ maze.py
@@ -102,8 +102,8 @@ maze = [[MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG
         [MG, F, F, F, F, F,MG, F, F,MG, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG],
         [MG, F, F, F, F, F,MG, F, F,MG, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG],
         [MG, F, F,MG,MG,MG,MG, F, F,MG, F, F,MG, F, F,MG, F, F,MG, F, F,MG, F, F,MG, F, F,MG, F, F,MG],
-        [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F,MG],
-        [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F,MG],
+        [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F],
+        [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F],
         [MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG]]
 
 
@@ -149,19 +149,7 @@ player_vy = 0
 player_speed = .25
 
 #make exit
-
-end = [29, 29, 2, 2]
-
-# Make coins
-def initialize_cups():
-    c_size = 1
-    cup1 = [23, 19, c_size, c_size]
-    cup2 = [3, 6, c_size, c_size]
-    cup3 = [21, 3, c_size, c_size]
-    cup4 = [24, 3, c_size, c_size]
-    
-    return [cup1, cup2, cup3, cup4]
-
+end = [28, 28, 2, 2]
 
 def draw_pixel(screen, color, a, b, pixel_size):
     pygame.draw.rect(screen, color, [a, b, pixel_size, pixel_size])
@@ -180,7 +168,7 @@ def draw_image(pixel_list, x, y, scale, pixel_size):
         b += pixel_size
         a = x * scale
   
-def draw_image2(pixel_list, x, y, scale, pixel_size, x_center, y_center):
+def spotlight(pixel_list, x, y, scale, pixel_size, x_center, y_center):
     
     radius = 3 * scale
 
@@ -198,135 +186,142 @@ def draw_image2(pixel_list, x, y, scale, pixel_size, x_center, y_center):
         b += pixel_size
         a = x * scale
 
+# stages
+START = 0
+PLAYING = 1
+END = 2
 
+def setup():
+    global player, stage, cups
+    
+    c_size = 2
+    cup1 = [4, 19, c_size, c_size]
+    cup2 = [28, 28, c_size, c_size]
+    cup3 = [16, 16, c_size, c_size]
+    cup4 = [28, 4, c_size, c_size]
+    
+    cups = [cup1, cup2, cup3, cup4]
 
+    player = [2, 1, 1.5, 1.5]
+    stage = START
 
-cups = initialize_cups()
-  
+font = pygame.font.Font(None, scale * 2)
 # Game loop
-win = False
+setup()
 done = False
 
 while not done:
     # Event processing (React to key presses, mouse clicks, etc.)
-    ''' for now, we'll just check to see if the X is clicked '''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+        elif event.type == pygame.KEYDOWN:
             
-    pressed = pygame.key.get_pressed()
+            if stage == START:
+                if event.key == pygame.K_SPACE:
+                    stage = PLAYING
+                  
+            elif stage == END:
+                if event.key == pygame.K_SPACE:
+                    setup()
 
-    up = pressed[pygame.K_UP]
-    down = pressed[pygame.K_DOWN]
-    left = pressed[pygame.K_LEFT]
-    right = pressed[pygame.K_RIGHT]
-    reset = pressed[pygame.K_r]
+                    
+    if stage == PLAYING:        
+        pressed = pygame.key.get_pressed()
 
-
-    if up:
-        player_vy = -player_speed
-    elif down:
-        player_vy = player_speed
-    else:
-        player_vy = 0
+        up = pressed[pygame.K_UP]
+        down = pressed[pygame.K_DOWN]
+        left = pressed[pygame.K_LEFT]
+        right = pressed[pygame.K_RIGHT]
         
-    if left:
-        player_vx = -player_speed
-    elif right:
-        player_vx = player_speed
-    else:
-        player_vx = 0
+        if up:
+            player_vy = -player_speed
+        elif down:
+            player_vy = player_speed
+        else:
+            player_vy = 0
+            
+        if left:
+            player_vx = -player_speed
+        elif right:
+            player_vx = player_speed
+        else:
+            player_vx = 0
 
-    if reset:
-        cups = initialize_cups()
-        win = False
-    
     # Game logic (Check for collisions, update points, etc.)
-    ''' move the player horizontally'''
-    player[0] += player_vx
+    if stage == PLAYING:
+        ''' move the player horizontally'''
+        player[0] += player_vx
+                
+        '''resolve collisions horizontally'''
+        for row_index, row in enumerate(maze):
+            for col_index, color in enumerate(row):
+                
+                r = [col_index, row_index, 1, 1]
+                
+                if intersects.rect_rect(player, r) and color != F:
+                    if player_vx > 0:
+                        player[0] = r[0] - player[2]      
+                    elif player_vx < 0:
+                        player[0] = r[0] + r[2]
+                            
+        ''' move the player in vertical direction '''
+        player[1] += player_vy
+           
+        ''' resolve collisions vertically'''
+        for row_index, row in enumerate(maze):
+            for col_index, color in enumerate(row):
+                
+                r = [col_index, row_index, 1, 1]
+                
+                if intersects.rect_rect(player, r) and color != F:
+                    if player_vy > 0:
+                        player[1] = r[1] - player[3]       
+                    elif player_vy < 0:
+                        player[1] = r[1] + r[3]
+                              
+        ''' here is where you should resolve player collisions with screen edges '''
+        top = player[1]
+        bottom = player[1] + player[3]
+        left = player[0]
+        right = player[0] + player[2]
 
-            
-    '''resolve collisions horizontally'''
-    for row_index, row in enumerate(maze):
-        for col_index, color in enumerate(row):
-            
-            r = [col_index, row_index, 1, 1]
-            
-            if intersects.rect_rect(player, r) and color != F:
-                if player_vx > 0:
-                    player[0] = r[0] - player[2]
-                    
-                elif player_vx < 0:
-                    player[0] = r[0] + r[2]
-               
-             
-    ''' move the player in vertical direction '''
-    player[1] += player_vy
-    
-    
-    ''' resolve collisions vertically'''
-    for row_index, row in enumerate(maze):
-        for col_index, color in enumerate(row):
-            
-            r = [col_index, row_index, 1, 1]
-            
-            if intersects.rect_rect(player, r) and color != F:
-                if player_vy > 0:
-                    player[1] = r[1] - player[3]
-                    
-                elif player_vy < 0:
-                    player[1] = r[1] + r[3]
-                    
-               
-               
-    ''' here is where you should resolve player collisions with screen edges '''
-    top = player[1]
-    bottom = player[1] + player[3]
-    left = player[0]
-    right = player[0] + player[2]
+        if left < 0:
+            player[0] = 0
+        elif right > WIDTH:
+            player[0] = WIDTH - player[2]
 
+        if top < 0:
+            player[1] = 0
+        elif bottom > HEIGHT:
+            player[1] = HEIGHT - player[3]
 
-   
-    if left < 0:
-        player[0] = 0
-    elif right > WIDTH:
-        player[0] = WIDTH - player[2]
-
-    if top < 0:
-        player[1] = 0
-    elif bottom > HEIGHT:
-        player[1] = HEIGHT - player[3]
-
-
-
-    ''' get the cups '''
-    
-    cups = [c for c in cups if not intersects.rect_rect(player, c)]
-
-    if len(cups) == 0 and intersects.rect_rect(player, end):
-        win = True
-
+        ''' get the cups '''        
+        cups = [c for c in cups if not intersects.rect_rect(player, c)]
+        if len(cups) == 0 and intersects.rect_rect(player, end):
+            stage = END
         
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
-    screen.fill(VDG)
+    screen.fill(VDG) 
 
-    
-    draw_image(harry, player[0], player[1], scale, 2)
-  
-    draw_image(maze, 0, 0, scale, scale)
-    '''draw_image2(maze, 0, 0, scale, scale, player[0], player[1])'''
-   
-    for c in cups:
-        draw_image(cup, c[0], c[1], scale, 2)
-    
-        
-    if win:
-        font = pygame.font.Font(None, scale * 2)
-        text = font.render("You Win!", 1, Y)
-        text_width = text.get_width()
-        text_height = text.get_height()
-        screen.blit(text, [WIDTH * scale / 2 - (text_width / 2), HEIGHT * scale / 2 - (text_height / 2)])
-        
+    if stage == PLAYING:
+        draw_image(harry, player[0], player[1], scale, 2)
+        draw_image(maze, 0, 0, scale, scale)
+        '''spotlight(maze, 0, 0, scale, scale, player[0], player[1])'''
+       
+        for c in cups:
+            draw_image(cup, c[0], c[1], scale, 3)
+            
+    if stage == END:
+        text1 = font.render("You Win!", True, Y)
+        text2 = font.render("You Win!", True, Y)
+        text_width1 = text1.get_width()
+        text_height1 = text1.get_height()
+        text_width2 = text2.get_width()
+        text_height2 = text2.get_height()
+        screen.blit(text1, [WIDTH * scale / 2 - (text_width1 / 2), HEIGHT * scale / 2 - (text_height1 / 2)])
+
     
     # Update screen (Actually draw the picture in the window.)
     pygame.display.flip()
