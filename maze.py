b7c4f5c8fac25c071e466a7f8cf94684fb1e382d
Zoe Stuart <zoestuart13@gmail.com>
2016-12-02 15:47:49 -0500

    Added score and multiple player option

--- maze.py
+++ maze.py
@@ -30,49 +30,20 @@ G = (160, 164, 165)
 GD = (96, 96, 96)
 B = (0, 0, 0)
 BR = (91, 59, 0)
+
 R = (198, 0, 0)
+DR = (100, 0, 0)
+O = (255, 148, 0)
 Y = (255, 208, 0)
-S = (255, 246, 188)
+S = (232, 193, 139)
 LG = (156, 203, 79)
 MG = (97, 144, 16)
 DG = (48, 75, 7)
 VDG = (37, 61, 25)
 LB = (26, 189, 239)
+BL = (147, 188, 255)
 
 F = None
-
-
-'''maze = [[MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG],
-        [ F, F, F, F, F, F, F, F, F, F, F, F,MG, F,MG, F,MG, F,MG, F, F, F, F, F, F, F, F, F, F, F,MG],
-        [MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG, F,MG, F,MG, F,MG,MG,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG],
-        [MG, F,MG, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG],
-        [MG, F,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG],
-        [MG, F,MG, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG, F,MG, F,MG, F, F, F, F, F,MG, F, F, F,MG],
-        [MG, F,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG, F,MG, F,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG],
-        [MG, F,MG, F, F, F,MG, F,MG, F,MG, F, F, F, F, F, F, F, F, F,MG, F, F, F,MG, F,MG, F,MG, F,MG],
-        [MG, F,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG, F,MG],
-        [MG, F, F, F,MG, F, F, F, F, F, F, F,MG, F, F, F,MG, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG],
-        [MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG,MG,MG],
-        [MG, F,MG, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F,MG, F, F, F, F, F,MG],
-        [MG, F,MG,MG,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG],
-        [MG, F, F, F, F, F,MG, F,MG, F, F, F,MG, F,MG, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG],
-        [MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG],
-        [MG, F,MG, F,MG, F, F, F,MG, F, F, F, F, F, F, F,MG, F, F, F, F, F, F, F, F, F,MG, F, F, F,MG],
-        [MG,MG,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG,MG,MG],
-        [MG, F,MG, F, F, F,MG, F,MG, F, F, F,MG, F,MG, F,MG, F, F, F,MG, F, F, F, F, F, F, F,MG, F,MG],
-        [MG, F,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG, F,MG, F,MG, F,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG],
-        [MG, F, F, F, F, F, F, F, F, F, F, F,MG, F,MG, F, F, F,MG, F,MG, F, F, F, F, F, F, F, F, F,MG],
-        [MG, F,MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG],
-        [MG, F, F, F,MG, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F,MG, F, F, F,MG, F, F, F,MG],
-        [MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG, F,MG, F,MG],
-        [MG, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG, F,MG],
-        [MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG, F,MG],
-        [MG, F, F, F, F, F, F, F,MG, F, F, F,MG, F, F, F, F, F,MG, F,MG, F, F, F, F, F,MG, F,MG, F,MG],
-        [MG, F,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG, F,MG,MG,MG,MG,MG, F,MG, F,MG,MG,MG, F,MG,MG,MG,MG,MG],
-        [MG, F, F, F,MG, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F,MG, F, F, F,MG],
-        [MG,MG,MG, F,MG, F,MG,MG,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG,MG,MG,MG,MG, F,MG,MG,MG, F,MG,MG,MG],
-        [MG, F, F, F, F, F,MG, F, F, F, F, F, F, F, F, F, F, F, F, F,MG, F, F, F, F, F, F, F, F, F, F],
-        [MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG]]'''
 
 maze = [[MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG],
         [MG, F, F, F, F, F, F, F, F, F, F, F, F, F, F,MG, F, F, F, F, F, F, F, F, F, F, F, F, F, F,MG],
@@ -105,7 +76,6 @@ maze = [[MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG
         [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F],
         [MG, F, F, F, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F, F, F,MG, F, F, F],
         [MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG,MG]]
-
 
 cup  = [[F, F, F, G, G, G, G, G, G, G, F, F, F],
         [ G, G, F, G,LB,LB,LB,LB,LB, G, F, G, G],
@@ -140,6 +110,66 @@ harry = [[F, F, F, B, B, B, B, B, B, B, B, F, F, F, F, F],
          [F, F, B, B,GD,GD,GD, B,GD,GD,GD, B, F, F, F, F],
          [F, F, B, B,GD,GD, B, B, B,GD,GD, B, F, F, F, F],
          [F, F, B, B,GD,GD, F, F, F,GD,GD, B, F, F, F, F]]
+
+cedric =[[F, F, F,BR,BR,BR,BR,BR,BR,BR,BR, F, F, F, F, F],
+         [F, F,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR, F, F, F, F],
+         [F,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR, F, F, F],
+         [F,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR,BR, F, F, F],
+         [F,BR,BR,BR,BR, S, S, S, S, S,BR,BR,BR, F, F, F],
+         [F,BR, S,BR, S, W, B, S, S, W, B, S,BR, F, F, F],
+         [F,BR,BR, S, S, S, S, S, S, S, S, S,BR, F, F, F],
+         [F, F, S, S, S, S, S, S, S, S, S, S, F, F, F, F],
+         [F, F, F, S, S, S, S, S, S, S, S, F, F, F, F,BR],
+         [F, F, B, G, Y, G, Y, G, Y, G, B, F, F, F, F,BR],
+         [F, B, B, Y, Y, G, Y, G, Y, G, B, B, F, F, F,BR],
+         [B, B, B, G, G, B, W, W, W, B, B, B, B, F, F,BR],
+         [B, B, B, Y, Y, B, B, B, B, B, Y, B, B, B, B, S],
+         [B, B, B, G, G, B, B, W, B, B, G, B, B, B, B,BR],
+         [B, B, B, Y, Y, B, W, W, W, B, B, B, F, B, B, F],
+         [S, S, B, B, B,GD,GD,GD,GD,GD, B, B, F, F, F, F],
+         [F, F, B, B,GD,GD,GD, B,GD,GD,GD, B, F, F, F, F],
+         [F, F, B, B,GD,GD, B, B, B,GD,GD, B, F, F, F, F],
+         [F, F, B, B,GD,GD, F, F, F,GD,GD, B, F, F, F, F]]
+
+krum =   [[F, F, F, B, B, B, B, B, B, B, B, F, F, F, F, F],
+         [F, F, B, B, B, B, B, B, B, B, B, B, F, F, F, F],
+         [F, B, B, B, B, B, B, B, B, B, B, B, B, F, F, F],
+         [F, B, B, B, B, B, B, B, B, B, B, B, B, F, F, F],
+         [F, B, B, B, S, S, S, S, S, S, S, B, B, F, F, F],
+         [F, B, B, S, S, W, B, S, S, W, B, S, B, F, F, F],
+         [F, B, S, S, S, S, S, S, S, S, S, S, B, F, F, F],
+         [F, B, B, S, S, S, S, S, S, S, S, S, B, F, F, F],
+         [F, F, F, F, S, S, S, S, S, S, S, F, F, F, F,BR],
+         [F, F, R, R,DR,DR,DR,DR,DR,BR,BR, F, F, F, F,BR],
+         [F, R, R, R,DR,DR,DR,DR, B,BR, R, R, F, F, F,BR],
+         [R, R, R, R,DR,DR, B, B,DR,BR, R, R, R, F, F,BR],
+         [R, R, R, R, B, B,DR,DR,DR,DR,BR, R, R, R, R, S],
+         [R, R, R, R, B, B, B, B, B, B,BR, R, R, R, R,BR],
+         [R, R, R, R,DR,DR,DR,DR,DR,DR,BR, R, F, F, F, F],
+         [S, S, R, R,DR, B, B, B, B, B,BR, R, R, F, F, F],
+         [F, F, R, R, B, B, B, R, B, B, B,BR, R, F, F, F],
+         [F, F, R, R, B, B, R, R, R, B, B,BR, R, F, F, F],
+         [F, F, R, R, B, B, F, F, F, B, B,BR, R, R, F, F]]
+
+fleur = [[ F, F, F, Y, Y, Y, Y, Y, Y, Y, Y, F, F, F, F, F],
+         [ F, F, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, F, F, F, F],
+         [ F, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, F, F, F],
+         [ F, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, Y, F, F, F],
+         [ F, Y, Y, Y, S, S, S, S, S, S, S, Y, Y, F, F, F],
+         [ F, Y, Y, S, S, W,LB, S, S, W,LB, S, Y, F, F, F],
+         [ F, Y, S, S, S, S, S, S, S, S, S, S, Y, F, F, F],
+         [ F, Y, Y, S, S, S, S, S, S, S, S, S, Y, F, F, F],
+         [ F, Y, Y, Y, S, S, S, S, S, S, S, Y, Y, F, F,BR],
+         [ F, Y,BL,BL,BL, W, W, W, W, W,BL, Y, Y, F, F,BR],
+         [ F,BL,BL,BL,BL,BL, W, W, W,BL,BL,BL, Y, F, F,BR],
+         [BL,BL,BL,BL,BL,BL, W, W, W,BL,BL,BL,BL, F, F,BR],
+         [BL,BL,BL,BL,BL,BL,BL,BL,BL,BL,BL,BL,BL,BL,BL, S],
+         [BL,BL,BL,BL,BL,BL,BL, W,BL,BL,BL,BL,BL,BL,BL,BR],
+         [BL,BL,BL,BL,BL,BL, W, W, W,BL,BL,BL, F, F, F, F],
+         [ S, S,BL,BL,BL, B, B, B, B, B,BL,BL, F, F, F, F],
+         [ F, F,BL,BL, B, B, B,BL, B, B, B,BL, F, F, F, F],
+         [ F, F,BL,BL, B, B,BL,BL,BL, B, B,BL, F, F, F, F],
+         [ F, F,BL,BL, B, B, F, F, F, B, B,BL, F, F, F, F]]
 
 
 # Make a player
@@ -205,8 +235,15 @@ def setup():
     player = [2, 1, 1.5, 1.5]
     stage = START
 
-font = pygame.font.Font(None, scale * 2)
+wizard = harry
+alt1 = krum
+alt2 = fleur
+alt3 = cedric
+
 # Game loop
+big_font = pygame.font.Font(None, scale * 2)
+small_font = pygame.font.Font(None, scale)
+score = 0
 setup()
 done = False
 
@@ -220,8 +257,27 @@ while not done:
             
             if stage == START:
                 if event.key == pygame.K_SPACE:
-                    stage = PLAYING
-                  
+                    stage = PLAYING  
+                elif event.key == pygame.K_k:
+                    wizard = krum
+                    alt1 = harry
+                    alt2 = fleur
+                    alt3 = cedric
+                elif event.key == pygame.K_h:
+                    wizard = harry
+                    alt1 = krum
+                    alt2 = fleur
+                    alt3 = cedric
+                elif event.key == pygame.K_f:
+                    wizard = fleur
+                    alt1 = harry
+                    alt2 = krum
+                    alt3 = cedric
+                elif event.key == pygame.K_c:    
+                    wizard = cedric
+                    alt1 = harry
+                    alt2 = fleur
+                    alt3 = krum
             elif stage == END:
                 if event.key == pygame.K_SPACE:
                     setup()
@@ -234,7 +290,7 @@ while not done:
         down = pressed[pygame.K_DOWN]
         left = pressed[pygame.K_LEFT]
         right = pressed[pygame.K_RIGHT]
-        
+
         if up:
             player_vy = -player_speed
         elif down:
@@ -298,29 +354,67 @@ while not done:
             player[1] = HEIGHT - player[3]
 
         ''' get the cups '''        
-        cups = [c for c in cups if not intersects.rect_rect(player, c)]
+        #coins = [c for c in coins if not intersects.rect_rect(player, c)]
+         
+        hit_list = [c for c in cups if intersects.rect_rect(player, c)]
+        
+        for hit in hit_list:
+            cups.remove(hit)
+            score += 1
+            print("sound!")
+            
         if len(cups) == 0 and intersects.rect_rect(player, end):
             stage = END
-        
+
+
     # Drawing code (Describe the picture. It isn't actually drawn yet.)
     screen.fill(VDG) 
 
+
+    if stage == START:
+        text1 = big_font.render("WELCOME TO THE TRIWIZARD MAZE!", True, Y)
+        text2 = small_font.render("perilous adventure awaits within...", True, MG)
+        text3 = small_font.render("press space to start", True, MG)
+        text_width1 = text1.get_width()
+        text_width2 = text2.get_width()
+        text_width3 = text3.get_width()
+        height = text1.get_height() + text2.get_height() + text3.get_height() 
+        screen.blit(text1, [388 - (text_width1 / 2), 388 - 35])
+        screen.blit(text2, [388 - (text_width2 / 2), 388 + 5])
+        screen.blit(text3, [388 - (text_width3 / 2), 388 + 25])
+
+        draw_image(wizard, 12.3, 4, scale, 10)
+        draw_image(cup, 2, 4, scale, 10)
+        draw_image(cup, 23.8, 4, scale, 10)
+        draw_image(alt1, 2, 20, scale, 10)
+        draw_image(alt2, 12.3, 20, scale, 10)
+        draw_image(alt3, 23.8, 20, scale, 10)
+
+
     if stage == PLAYING:
-        draw_image(harry, player[0], player[1], scale, 2)
+        draw_image(wizard, player[0], player[1], scale, 2)
         draw_image(maze, 0, 0, scale, scale)
         '''spotlight(maze, 0, 0, scale, scale, player[0], player[1])'''
        
         for c in cups:
             draw_image(cup, c[0], c[1], scale, 3)
+
+        text = small_font.render( "score: " + str(score), True, Y)
+        screen.blit(text, [28 * scale, 0]) 
             
     if stage == END:
-        text1 = font.render("You Win!", True, Y)
-        text2 = font.render("You Win!", True, Y)
+        text1 = big_font.render("You Win!", True, Y)
+        text2 = small_font.render("press space to reset", True, MG)
         text_width1 = text1.get_width()
-        text_height1 = text1.get_height()
         text_width2 = text2.get_width()
+        text_height1 = text1.get_height()
         text_height2 = text2.get_height()
-        screen.blit(text1, [WIDTH * scale / 2 - (text_width1 / 2), HEIGHT * scale / 2 - (text_height1 / 2)])
+        screen.blit(text1, [388 - (text_width1 / 2), 388 - text_height1])
+        screen.blit(text2, [388 - (text_width2 / 2), 388 + text_height1 / 2])
+
+        draw_image(wizard, 12.3, 4, scale, 10)
+        draw_image(cup, 2, 4, scale, 10)
+        draw_image(cup, 23.8, 4, scale, 10)
 
     
     # Update screen (Actually draw the picture in the window.)
